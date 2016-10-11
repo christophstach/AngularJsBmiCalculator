5a0734204c93e76fb7b79b9472b886d793e89177
Christoph Stach <christoph.stach@gmail.com>
2016-10-11 13:08:18 +0200

    Erste Anfänge des BMI Calculators mit Semantic UI und AngularJS 2

---
+++ src/app/bmi-form/bmi-form.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { BmiFormComponent } from './bmi-form.component';
+
+describe('Component: BmiForm', () => {
+  it('should create an instance', () => {
+    let component = new BmiFormComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/bmi-form/bmi-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bmi-form',
+  templateUrl: './bmi-form.component.html',
+  styleUrls: ['./bmi-form.component.scss']
+})
+export class BmiFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
