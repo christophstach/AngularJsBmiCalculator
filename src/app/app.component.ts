44a3d599ab910075b9f66372b63d11d6a6e146b8
Christoph Stach <christoph.stach@gmail.com>
2016-10-11 15:12:38 +0200

    Titel der App geändert.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Das ist der geile BMI Calculatory';
 }
